8b02373dca95c0e523582ffe41b5c9c13aac401f
Arun-mec <84711871+Arun-mec@users.noreply.github.com>
2022-09-29 09:59:10 +0530

    edited employee exception

--- EmployeeDetailsException.java
+++ EmployeeDetailsException.java
@@ -3,24 +3,26 @@ package com.nissan.bean;
 import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-import com.nissan.bean.CustomerException;
+import com.nissan.bean.AgeException;
 
 public class EmployeeDetailsException {
 	public static void main(String[] args) throws Exception {
 		
 		try {
-		
+		// Declaring the variables
 		Scanner input = new Scanner(System.in);
 		String empName = "";
 		int empAge = 0;
 		char choice = 'n';
 		
 		do {
+			// Prompt input from the user
 			System.out.println("Enter the employee name:");
 			empName = input.next();
 			System.out.println("Enter the employee age:");
 			empAge = input.nextInt();
 			
+			// checking the age of the user
 			if (empAge>60 || empAge<18) {
 				throw new AgeException();
 			}
@@ -31,6 +33,7 @@ public class EmployeeDetailsException {
 		}while (choice=='y' || choice=='Y');
 		
 		}
+		// Catching the age exception
 		catch(AgeException e) {
 			System.out.println("Message"+e);
 		}
